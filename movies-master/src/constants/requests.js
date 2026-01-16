b69029457259c3e04f0fc10a571550465febfbb5
AHelijao <helijao.andriamandranto@gmail.com>
2026-01-16 18:44:12 +0100

    assessment2

--- movies-master/src/constants/requests.js
+++ movies-master/src/constants/requests.js
@@ -8,3 +8,26 @@ export const movieRequests = {
   fetchAllMovies: '/movies',
   fetchNetflixOriginals: '/movies?category=Netflix Originals',
 }
+
+export const addMovie = '/movies';
+try {
+  const newMovie = new Movie({
+    title,
+    release_date,
+    author,
+    type,
+    poster,
+    backdrop_poster,
+    overview,
+    creation_date: req.body.creation_date,
+  });
+
+  const savedMovie = await newMovie.save();
+  logger.info(savedMovie);
+  res.status(200).json({ message: "Movie added", movie: savedMovie }); // Assuming success is 200
+} catch (err) {
+  logger.error(err.stack);
+  res
+    .status(500) // Assuming queryError is 500
+    .json({ error: "Exception occured while adding new movie" });
+}
--- movies-master/src/pages/MovieDetailsPage.js
+++ movies-master/src/pages/MovieDetailsPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetchData from '../hooks/useFetchData'
 
 const MovieDetailsPage = () => {
+  const { movieId } = useParams();
+  const [movie] = useFetchData(`/movies/${movieId}`);
+
   return (
     <div>
-      MovieDetailsPage
+      {movie.title}
     </div>
   )
 }
